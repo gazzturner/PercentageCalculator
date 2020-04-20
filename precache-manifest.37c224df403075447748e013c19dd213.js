6672f002e4a1205beeccff63447cfd013b86a723
Garry <garry@evolving.technology>
2020-04-20 16:29:11 +0100

    Updates

---
+++ precache-manifest.37c224df403075447748e013c19dd213.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65661780319482ebcddfe6498d435cfc",
+    "url": "/gazzturner/PercentageCalculator/index.html"
+  },
+  {
+    "revision": "997e9af38b4dc29c53c5",
+    "url": "/gazzturner/PercentageCalculator/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "59dcf8ab5493ea1350fb",
+    "url": "/gazzturner/PercentageCalculator/static/js/2.9f0cc851.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/gazzturner/PercentageCalculator/static/js/2.9f0cc851.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "997e9af38b4dc29c53c5",
+    "url": "/gazzturner/PercentageCalculator/static/js/main.e5b1f4e7.chunk.js"
+  },
+  {
+    "revision": "7c63c22f1298c952cd5a",
+    "url": "/gazzturner/PercentageCalculator/static/js/runtime-main.64fc8436.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/gazzturner/PercentageCalculator/static/media/logo.5d5d9eef.svg"
+  }
+]);
