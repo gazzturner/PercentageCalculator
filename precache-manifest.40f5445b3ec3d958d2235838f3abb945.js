c6443276a864681b515a10f63773809d85f2afba
Garry <garry@evolving.technology>
2020-04-20 16:35:20 +0100

    Updates

---
+++ precache-manifest.40f5445b3ec3d958d2235838f3abb945.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9c7e4455c679ebd662769b5c51d9f74",
+    "url": "/PercentageCalculator/index.html"
+  },
+  {
+    "revision": "5b4e69c0a620ec75e7cc",
+    "url": "/PercentageCalculator/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "59dcf8ab5493ea1350fb",
+    "url": "/PercentageCalculator/static/js/2.9f0cc851.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/PercentageCalculator/static/js/2.9f0cc851.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b4e69c0a620ec75e7cc",
+    "url": "/PercentageCalculator/static/js/main.38786b40.chunk.js"
+  },
+  {
+    "revision": "f898b727fcadca4d1f06",
+    "url": "/PercentageCalculator/static/js/runtime-main.9aa6c162.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/PercentageCalculator/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/gazzturner/PercentageCalculator/precache-manifest.37c224df403075447748e013c19dd213.js"
+  "/PercentageCalculator/precache-manifest.40f5445b3ec3d958d2235838f3abb945.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/gazzturner/PercentageCalculator/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/PercentageCalculator/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
