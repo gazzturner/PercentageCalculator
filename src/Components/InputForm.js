1d1ad745f6b880f9b9e110a1516b07d6d1a305fd
Garry <garry@evolving.technology>
2020-04-21 09:24:01 +0100

    added extra variables for percentage taken and added to form

--- src/Components/InputForm.js
+++ src/Components/InputForm.js
@@ -9,18 +9,30 @@ constructor(props) {
       percentageTaken: null,
       additionalPayments: []
     };
+    let amountForClient = null;
   }
   
-  onChange = (event) => {
+  setTotal = (event) => {
     this.setState({ total: event.target.value });
+  }
+
+  setPercentage = (event) => {
+    this.setState({ percentageTaken: event.target.value });
+  }
+
+  generatePercentage = () => {
+    var amountTaken = (this.state.total/100) * this.state.percentageTaken;
+    amountForClient = this.state.total - amountTaken
   }
 
   render(){
         return(
             <>
                 <div>Hello from form 2</div>
-                <input value={this.state.total} onChange={this.onChange}></input>
-                {this.state.total}
+                <input value={this.state.total} onChange={this.setTotal}></input><br/>
+                <input value={this.state.percentageTaken} onChange={this.setPercentage}></input><br/>
+                <button onClick={this.generatePercentage}>Generate</button><br />
+                {this.amountForClient}
             </>
         );
     };
