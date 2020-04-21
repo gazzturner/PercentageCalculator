8f4f98122fc189c068bb8c998d7b1448e9e76a74
Garry <garry@evolving.technology>
2020-04-21 10:34:20 +0100

    added maths and additional payments function

--- src/Components/InputForm.js
+++ src/Components/InputForm.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
-
+import ResultForm from './ResultForm'
+import AdditionalPayments from './AdditionalPayments';
 
 export default class InputForm extends Component {
 constructor(props) {
@@ -7,9 +8,10 @@ constructor(props) {
     this.state = {
       total: null,
       percentageTaken: null,
-      additionalPayments: []
+      additionalPayments: [],
+      amountForClient: null,
+      newAdditionalPayment: null
     };
-    let amountForClient = null;
   }
   
   setTotal = (event) => {
@@ -22,18 +24,50 @@ constructor(props) {
 
   generatePercentage = () => {
     var amountTaken = (this.state.total/100) * this.state.percentageTaken;
-    amountForClient = this.state.total - amountTaken
+    var additionalPaymentAmount = 0;
+    this.state.additionalPayments.forEach(payment => {
+      additionalPaymentAmount += parseFloat(payment);
+    });
+    amountTaken += parseFloat(additionalPaymentAmount);
+    this.setState({amountForClient: this.state.total - amountTaken}); 
+  }
+
+  setAdditionalPayment = (event) => {
+    this.setState({newAdditionalPayment: event.target.value});
+  }
+
+  addAdditionalPayment = () => {
+    let updatedAdditionalPayments = this.state.additionalPayments;
+    updatedAdditionalPayments.push(this.state.newAdditionalPayment);
+    this.setState({newAdditionalPayment: null});
+    this.setState({additionalPayments: updatedAdditionalPayments}); 
+  }
+
+  resetForm = () =>  {
+    window.location.reload();
   }
 
   render(){
+    let additionalPayments = null;
+    additionalPayments = this.state.additionalPayments.map((payment, index) => {
+      return(<AdditionalPayments
+        additionalPayment={payment}
+        />
+      )
+    });
+
         return(
-            <>
-                <div>Hello from form 2</div>
-                <input value={this.state.total} onChange={this.setTotal}></input><br/>
-                <input value={this.state.percentageTaken} onChange={this.setPercentage}></input><br/>
+            <div>
+                Enter Total Damages: £<input value={this.state.total} onChange={this.setTotal} /><br/>
+                Enter Percentage Taken: <input value={this.state.percentageTaken} onChange={this.setPercentage} />%<br/>
+                Add An Additional Payment: <input value={this.state.newAdditionalPayment} onChange={this.setAdditionalPayment}/> <button onClick={this.addAdditionalPayment}>+</button><br />
+                
+                Additional Payments: {additionalPayments}<br />
                 <button onClick={this.generatePercentage}>Generate</button><br />
-                {this.amountForClient}
-            </>
+                <ResultForm finalAmount={this.state.amountForClient} />
+
+                <button onClick={this.resetForm}>Reset Form</button>
+            </div>
         );
     };
 };
---
+++ src/Components/ResultForm.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ResultForm = (props) => {
+    return (
+        <div>Amount for client: £{props.finalAmount}</div>
+    );
+};
+
+export default ResultForm;
